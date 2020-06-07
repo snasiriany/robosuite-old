c2d532b498835e22ee79b86bd915755a4806f253
Danfei Xu <morphine1021@gmail.com>
2020-06-07 10:08:24 -0700

    expert policy and envs for LGP

--- robosuite/environments/sawyer_playtable.py
+++ robosuite/environments/sawyer_playtable.py
@@ -261,6 +261,25 @@ class SawyerPT(SawyerEnv):
 
 
 class SawyerPTStack(SawyerPT):
+    def _load_objects(self):
+        # setup objects and initializers
+        mujoco_objects = OrderedDict()
+        visual_objects = OrderedDict()
+
+        mujoco_objects["cube1"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(1, 0, 0, 1))
+        mujoco_objects["cube2"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(0, 0, 1, 1))
+        mujoco_objects["plate"] = BoxObject(size=(0.03, 0.03, 0.01), rgba=(0, 1, 0, 1))
+
+        visual_objects["cube1_visual"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(1, 0, 0, 0.3))
+        visual_objects["cube2_visual"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(0, 0, 1, 0.3))
+        visual_objects["plate_visual"] = BoxObject(size=(0.03, 0.03, 0.01), rgba=(0, 1, 0, 0.3))
+        # target visual object
+        return mujoco_objects, visual_objects
+
+    @property
+    def task_object_names(self):
+        return ["cube1", "cube2", "plate"]
+
     def _get_default_placement_initializer(self):
         initializer = SequentialCompositeSampler()
         initializer.sample_on_top(
@@ -280,41 +299,6 @@ class SawyerPTStack(SawyerPT):
         initializer.hide("cube2_visual")
         initializer.hide("plate_visual")
         return initializer
-
-    def _get_placement_initializer_for_eval_mode(self):
-        initializer = SequentialCompositeSampler()
-        initializer.sample_on_top_square_grid(
-            "cube1", "table",
-            x_bound=(-0.1, 0.1, 5), y_bound=(-0.1, 0.1, 5), z_bound=(0.0, 0.0, 1), ensure_object_boundary_in_range=False
-        )
-        initializer.sample_on_top_square_grid(
-            "cube2", "table",
-            x_bound=(-0.1, 0.1, 5), y_bound=(-0.1, 0.1, 5), z_bound=(0.0, 0.0, 1), ensure_object_boundary_in_range=False
-        )
-        initializer.sample_on_top_square_grid(
-            "plate", "table",
-            x_bound=(-0.1, 0.1, 5), y_bound=(-0.1, 0.1, 5), z_bound=(0.0, 0.0, 1), ensure_object_boundary_in_range=False
-        )
-        return initializer
-
-    def _load_objects(self):
-        # setup objects and initializers
-        mujoco_objects = OrderedDict()
-        visual_objects = OrderedDict()
-
-        mujoco_objects["cube1"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(1, 0, 0, 1))
-        mujoco_objects["cube2"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(0, 0, 1, 1))
-        mujoco_objects["plate"] = BoxObject(size=(0.03, 0.03, 0.01), rgba=(0, 1, 0, 1))
-
-        visual_objects["cube1_visual"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(1, 0, 0, 0.3))
-        visual_objects["cube2_visual"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(0, 0, 1, 0.3))
-        visual_objects["plate_visual"] = BoxObject(size=(0.03, 0.03, 0.01), rgba=(0, 1, 0, 0.3))
-        # target visual object
-        return mujoco_objects, visual_objects
-
-    @property
-    def task_object_names(self):
-        return ["cube1", "cube2", "plate"]
 
     @property
     def task_id(self):
@@ -417,3 +401,148 @@ class SawyerPTStackAll(SawyerPTStack):
             self.sim.model.body_name2id("plate"),
         )
         return cube_stack and cube_on_plate
+
+
+class Pose(object):
+    def __init__(self, pos=None, rot=None):
+        self.pos = np.array(pos) if pos is not None else None
+        self.rot = np.array(rot) if rot is not None else None
+
+    @property
+    def array(self):
+        return np.concatenate((self.pos, self.rot), axis=0)
+
+
+def object_pose(env, name):
+    init_pos = np.array(env.sim.data.geom_xpos[env.sim.model.geom_name2id(name)])
+    init_quat = np.array(env.sim.data.geom_xpos[env.sim.model.geom_name2id(name)])
+    return Pose(init_pos, init_quat)
+
+
+def point_to_line_segment(p, q1, q2):
+    pq1 = p - q1
+    pq2 = p - q2
+    q2q1 = q2 - q1
+    q1q2 = q1 - q2
+
+    def angle(x, y):
+        return np.arccos(np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y)))
+
+    if angle(pq1, q2q1) > (0.5 * np.pi):
+        return np.linalg.norm(pq1)
+    if angle(pq2, q1q2) > (0.5 * np.pi):
+        return np.linalg.norm(pq2)
+
+    return np.sin(angle(pq1, q2q1)) * np.linalg.norm(pq1)
+
+
+def check_path_collisions(env, start_pose, end_pose, eef_collision_radius):
+    """
+    Find all task objects that would collide with a cartesian path
+    Args:
+        start_pose (Pose): (pos, ori)
+        end_pose (Pose): (pos, ori)
+        eef_collision_radius (float): radius for checking collisions in 3D
+
+    Returns:
+        set of body names that will collide w/ the eef
+
+    """
+    start_pos = start_pose.pos
+    end_pos = end_pose.pos
+    collides = []
+    for tn in env.task_object_names:
+        op = object_pose(env, tn)
+        if point_to_line_segment(op.pos, start_pos, end_pos) < eef_collision_radius:
+            collides.append(tn)
+    return collides
+
+
+class SawyerPTLGP(SawyerPT):
+    @property
+    def task_id(self):
+        return 0.
+
+    def _get_default_placement_initializer(self):
+        initializer = SequentialCompositeSampler()
+        initializer.sample_on_top(
+            "cube1", "table",
+            x_range=(-0.07, 0.07), y_range=(-0.1, 0.1), z_rotation=(0.0, 0.0), ensure_object_boundary_in_range=False
+        )
+        initializer.sample_on_top(
+            "cube2", "table",
+            x_range=(-0.07, 0.07), y_range=(-0.1, 0.1), z_rotation=(0.0, 0.0), ensure_object_boundary_in_range=False
+        )
+        initializer.sample_on_top(
+            "cube3", "table",
+            x_range=(-0.07, 0.07), y_range=(-0.1, 0.1), z_rotation=(0.0, 0.0),  ensure_object_boundary_in_range=False
+        )
+        initializer.sample_on_top(
+            "cube4", "table",
+            x_range=(-0.07, 0.07), y_range=(-0.1, 0.1), z_rotation=(0.0, 0.0), ensure_object_boundary_in_range=False
+        )
+        # initializer.sample_on_top(
+        #     "cube5", "table",
+        #     x_range=(-0.1, 0.1), y_range=(-0.2, 0.2), z_rotation=(0.0, 0.0), ensure_object_boundary_in_range=False
+        # )
+        # initializer.sample_on_top(
+        #     "cube6", "table",
+        #     x_range=(-0.1, 0.1), y_range=(-0.2, 0.2), z_rotation=(0.0, 0.0),  ensure_object_boundary_in_range=False
+        # )
+
+        initializer.hide("cube1_visual")
+        initializer.hide("cube2_visual")
+        initializer.hide("cube3_visual")
+        initializer.hide("cube4_visual")
+        # initializer.hide("cube5_visual")
+        # initializer.hide("cube6_visual")
+        return initializer
+
+    def _load_objects(self):
+        # setup objects and initializers
+        mujoco_objects = OrderedDict()
+        visual_objects = OrderedDict()
+
+        mujoco_objects["cube1"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(1, 0, 0, 1))
+        mujoco_objects["cube2"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(0, 1, 0, 1))
+        mujoco_objects["cube3"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(0, 0, 1, 1))
+        mujoco_objects["cube4"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(1, 0, 1, 1))
+        # mujoco_objects["cube5"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(0, 1, 1, 1))
+        # mujoco_objects["cube6"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(1, 1, 0, 1))
+
+        visual_objects["cube1_visual"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(1, 0, 0, 0.3))
+        visual_objects["cube2_visual"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(0, 1, 0, 0.3))
+        visual_objects["cube3_visual"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(0, 0, 1, 0.3))
+        visual_objects["cube4_visual"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(1, 0, 1, 0.3))
+        # visual_objects["cube5_visual"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(0, 1, 1, 0.3))
+        # visual_objects["cube6_visual"] = BoxObject(size=(0.02, 0.02, 0.02), rgba=(1, 1, 0, 0.3))
+        # target visual object
+        return mujoco_objects, visual_objects
+
+    def _reset_internal(self):
+        ret = super(SawyerPTLGP, self)._reset_internal()
+        self.target_pos = np.array(self.sim.data.body_xpos[self.sim.model.body_name2id("cube2")])
+        return ret
+
+    @property
+    def task_object_names(self):
+        return ["cube1", "cube2", "cube3", "cube4"]
+
+    def _set_state_to_goal(self):
+        """Set the environment to a goal state"""
+        p1 = object_pose(self, "cube1")
+        p2 = object_pose(self, "cube2")
+        obstacles = [tn for tn in check_path_collisions(self, p1, p2, 0.05) if tn != "cube1"]
+        if "cube2" not in obstacles:
+            obstacles.append("cube2")
+
+        for tn in obstacles:
+            opose = object_pose(self, tn)
+            opose.pos[0] = 0.7
+            self.sim.forward()
+        EU.set_body_pose(self.sim, "cube1", self.target_pos)
+        self.sim.forward()
+
+    def _check_success(self):
+        cube1_pos = np.array(self.sim.data.body_xpos[self.sim.model.body_name2id("cube1")])
+        return np.linalg.norm(cube1_pos - self.target_pos) < 0.03
